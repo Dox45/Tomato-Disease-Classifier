3b09f7bb96f13945fccc8373f2ca6a0ff392fd74
purpleWavelet <fender54eurois@gmail.com>
2025-09-18 13:28:39 +0100

    Update next.config.ts

--- tomatoui/tomato/next.config.ts
+++ tomatoui/tomato/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // ❌ Don’t run ESLint during builds
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // ❌ Don’t block production builds on TS errors
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
